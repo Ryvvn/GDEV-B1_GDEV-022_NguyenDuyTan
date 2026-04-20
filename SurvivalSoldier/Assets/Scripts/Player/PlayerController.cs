7c0d65356e15c404a72118e5125fd405fcf3c9ab
AugustanHalo <tannd1008@gmail.com>
2026-04-20 09:57:26 +0700

    feat: add shooting

--- SurvivalSoldier/Assets/Scripts/Player/PlayerController.cs
+++ SurvivalSoldier/Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
@@ -9,6 +10,13 @@ namespace Player
         public float speed = 10f;
 
         private Vector3 target;
+        private Vector3 direction;
+        [SerializeField] float shootInterval = 0.2f;
+        private float lastShoot;
+        private bool canShoot = true;
+
+
+        [SerializeField] private GameObject Bullet;
         private void Update()
         {
             if(target != null && target != Vector3.zero)
@@ -23,7 +31,25 @@ namespace Player
                     target.z = transform.position.z;
                 }
             }
+            lastShoot += Time.deltaTime;
+            if (lastShoot < shootInterval)
+            {
+                canShoot = true;
+            }
+            else
+            {
+                canShoot = false;
+            }
+
+            if(Input.GetKeyDown("space") && canShoot)
+            {
+                direction = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                direction.z = transform.position.z;
+                Shoot(direction);
+            }
         }
+
+      
         private void MoveTowardTarget(Vector3 target)
         {
             transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
@@ -34,6 +60,16 @@ namespace Player
                     target = Vector3.zero;
                 }
             }
+        }
+        private void Shoot(Vector3 target)
+        {
+          
+            GameObject bullet = Instantiate(Bullet, transform.position, Quaternion.identity);
+
+            bullet.GetComponent<Rigidbody2D>().velocity = target;
+
+            lastShoot = 0;
+            
         }
     }
 }
